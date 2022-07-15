792487e9fa58cb46a82fda5388efbec79d30662b
phamthainb <phamthai4643@gmail.com>
2022-07-15 22:14:06 +0700

    [master] update FE base

--- front-end/src/redux/root-reducers.ts
+++ front-end/src/redux/root-reducers.ts
@@ -4,21 +4,16 @@
  * phamthainb
  */
 
-import { combineReducers } from "redux";
-import App from "containers/App/store/reducers";
-import Home from "containers/Home/store/reducers";
-import About from "containers/About/store/reducers";
+import App from 'containers/App/store/reducers';
+import Home from 'containers/Home/store/reducers';
+import { combineReducers } from 'redux';
 // [IMPORT NEW CONTAINERSTATE ABOVE] < Needed for generating containers seamlessly
-
-
 
 //pages
 const rootReducer = combineReducers({
   App,
   Home,
-  About,
   // [INSERT NEW REDUCER KEY ABOVE] < Needed for generating containers seamlessly
-
-  });
+});
 
 export default rootReducer;
